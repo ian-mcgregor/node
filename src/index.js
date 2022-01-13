ec4e53f3da9c096cbd88e4cfbfd5e0338d5dba5c
Ian McGregor <ian.mcgregor@colorado.edu>
2022-01-13 16:59:37 -0700

    Added a few thangs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000
 
+// Object containing a print helper function
+// Displays syntax for defining properties of an object, including functions.
+const event = {
+    x : '',
+    print() { 
+        console.log(this.x)
+    }
+}
+
+// GET
 app.get('', (req, res) =>{
-    res.send('<br><br>Hello<br><br>World')
+    // read html from src
+    var indexHtml = fs.readFileSync('src/index.html')
+
+    // pass html to resolver as string
+    res.sendFile(__dirname + "/index.html")
+
+    // set 'x' property of event object to desired message and print
+    event.x = 'Inside GET Request'
+    event.print()
 })
 
+// LISTENER
 app.listen(port, () => {
     console.log('Server is up on port '+ port)
 })
